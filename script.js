1fd6dfc483751c06796719bdad9b72055f3b9e72
pulakkar01 <pulakkar.mysoftheaven@gmail.com>
2023-12-05 12:29:19 +0600

    first_comment

---
+++ script.js
@@ -0,0 +1,45 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+	menuIcon.classList.toggle('bx-x');
+	navbar.classList.toggle('active');
+};
+
+
+// ------------------// scroll sections active-------------
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+	sections.forEach(sec => {
+		let top = window.scrollY;
+		let offset = sec.offsetTop - 150;
+		let height = sec.offsetHeight;
+		let id = sec.getAttribute('id');
+
+		if (top >= offset && top < offset + height) {
+			navlinks.forEach(links => {
+				links.classList.remove('active');
+				document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+			});
+		};
+	});
+
+	// remove onclick
+
+	menuIcon.classList.remove('bx-x');
+	navbar.classList.remove('active');
+};
+
+// scroll revel
+
+ScrollReveal({
+	reset: true,
+	distance: '80px',
+	durattion: 2000,
+	delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-containet, .portfolio-box, .contact form', { origin: 'bottom' });
